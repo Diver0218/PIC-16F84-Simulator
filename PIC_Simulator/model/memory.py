ce8e3bf535ad90a6fa5e86316048c781dac48e67
Diver0218 <jamin.bareis@gmail.com>
2024-03-07 09:13:38 +0100

    grobe Struktur vom Speicher erstellt

--- PIC_Simulator/model/memory.py
+++ PIC_Simulator/model/memory.py
@@ -1,28 +1,4 @@
-class Register():
-
-    value : int
-
-    def __init__(self):
-        self.set(0x00)
-
-    def set(self, value : int):
-        if value > 0xFF:
-            self.set(value - 0xFF)
-            # set overload flagg
-        else:
-            self.value = value
-    
-    def increment(self):
-        self.value += 1
-        if self.value > 0xFF:
-            self.value = 0x00
-            # set overload flagg
-
-    def decrement(self):
-        self.value -= 1
-        if self.value < 0x00:
-            self.value = 0xFF
-            # set overload flagg
+from .registers import Register
 
 class Memory():
 
@@ -52,10 +28,6 @@ class Memory():
 
     stackpointer : int
 
-    stack : int
+    stack : int = [0] * 68
 
-
-mem = Memory()
-
-print(mem.eeprom)
 
---
+++ PIC_Simulator/model/registers.py
@@ -0,0 +1,25 @@
+class Register():
+
+    value : int
+
+    def __init__(self):
+        self.set(0x00)
+
+    def set(self, value : int):
+        if value > 0xFF:
+            self.set(value - 0xFF)
+            # set overload flagg
+        else:
+            self.value = value
+    
+    def increment(self):
+        self.value += 1
+        if self.value > 0xFF:
+            self.value = 0x00
+            # set overload flagg
+
+    def decrement(self):
+        self.value -= 1
+        if self.value < 0x00:
+            self.value = 0xFF
+            # set overload flagg
