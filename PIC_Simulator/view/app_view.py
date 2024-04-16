5a1beec6c2db368e472d5dba8c047b38bde0bd8e
Benedikt Herold <benne.herold@gmx.de>
2024-04-16 10:21:00 +0200

    view Structure Update -> Main, Window, Ports filled etc.

--- PIC_Simulator/view/app_view.py
+++ PIC_Simulator/view/app_view.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QTableWidget, QTableWidgetItem, QMenuBar, QMenu, QMainWindow
+from PyQt6.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QTableWidget, QTableWidgetItem, QMenuBar, QMenu, QMainWindow, QSizePolicy
 from PyQt6.QtWidgets import QLineEdit
 import sys
 
@@ -9,37 +9,46 @@ from lst_parser_bits import Listing
 
 class MemTable(QTableWidget):
 
-    data : list
-
-    def __init__(self, data, *args):
+    def __init__(self, *args):
         QTableWidget.__init__(self, *args)
-        self.data = data
         self.resizeColumnsToContents()
         self.resizeRowsToContents()
 
-    def setData(self, data):
-        horHeaders = ['Adress', 'Bit 0', 'Bit 1', 'Bit 2', 'Bit 3', 'Bit 4', 'Bit 5', 'Bit 6', 'Bit 6', 'Full Value']
+    def setData(self, mem):
         rows = self.rowCount()
         columns = self.columnCount()
-        adress = 0
+        verticaHeaders = []
         for i in range(rows):
             for j in range(columns):
-                if j == 0:
-                    item = adress
-                    newitem = QTableWidgetItem(str(item))
-                    self.setItem(i , j, newitem)
-                    adress += 1
-                elif j == 9:
-                    item = data[i].value
+                if j == 8:
+                    item = mem[i].value
                     newitem = QTableWidgetItem(str(item))
                     self.setItem(i , j, newitem)
                 else:
-                    item = data[i].test_bit(j - 1)
+                    item = mem[i].test_bit(j)
                     newitem = QTableWidgetItem(str(item))
                     self.setItem(i , j, newitem)
-        self.setHorizontalHeaderLabels(horHeaders)
+            verticaHeaders.append(str(hex(i))[2:])
+        self.setVerticalHeaderLabels(verticaHeaders)
+
     
-class MainWindow(QWidget):
+    def setPortData(self, mem, adr):
+        rows = self.rowCount()
+        columns = self.columnCount()
+        for i in range(columns):
+            self.setItem(2, i, QTableWidgetItem(str(mem[adr].test_bit(7 - i))))
+        for i in range(columns):
+            item = mem[adr].test_bit(7 - i) #adr muss noch um +0x80 verschoben werden
+            self.setItem(0, i, QTableWidgetItem(str(item)))
+            if item:
+                self.setItem(1, i, QTableWidgetItem('i'))
+            else:
+                self.setItem(1, i, QTableWidgetItem('o'))
+        
+            
+    
+    
+class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
@@ -50,61 +59,74 @@ class MainWindow(QWidget):
         self.lst = Listing()
         self.p = Processor(self.lst.get_instructions())
 ##########################
-        self.lay_main = QHBoxLayout()
-        self.lay_reg = QVBoxLayout()
-        self.lay_code = QVBoxLayout()
-        self.lay_runctrl = QVBoxLayout()
-        self.lay_brk = QHBoxLayout()
-        self.lay_freq = QHBoxLayout()
-        self.lbl_code = QLabel("lskdugaldkgjsdlgkjbasdjgkbsdjgdfgdGSDGsdds\nwefwefewfwefwefwefWEFWefWEGWegewG\n hfgduzsgkeriugziebsztieruztvgerzuvteuzteriuvziguzrgiuzrgkazgrzaergkzeragkreuz")
-        self.btn_step = QPushButton('Step')
-        self.btn_run = QPushButton('Run')
-        self.btn_stop = QPushButton('Stop')
-        self.btn_reset = QPushButton('Reset')
-        self.txtbox_brk = QLineEdit("-")
-        self.txtbox_freq = QLineEdit("4.0")
-        self.btn_setbrk = QPushButton('Set')
-        self.btn_setfreq = QPushButton('Set')
-        self.lbl_timer = QLabel("0us")
-        self.tbl_porta = MemTable("test", 80, 10)
-        self.tbl_portb = MemTable("test", 80, 10)
-        self.tbl_mem = MemTable("test", 80, 10)
-        self.lbl_sfr = QLabel("SFR")
-        self.tableData : list
+        widg_main = QWidget()
+        lay_main = QHBoxLayout()
+        lay_reg = QVBoxLayout()
+        lay_code = QVBoxLayout()
+        lay_runctrl = QVBoxLayout()
+        lay_brk = QHBoxLayout()
+        lay_freq = QHBoxLayout()
+        lbl_code = QLabel("lskdugaldkgjsdlgkjbasdjgkbsdjgdfgdGSDGsdds\nwefwefewfwefwefwefWEFWefWEGWegewG\n hfgduzsgkeriugziebsztieruztvgerzuvteuzteriuvziguzrgiuzrgkazgrzaergkzeragkreuz")
+        btn_step = QPushButton('Step')
+        btn_run = QPushButton('Run')
+        btn_stop = QPushButton('Stop')
+        btn_reset = QPushButton('Reset')
+        txtbox_brk = QLineEdit("-")
+        txtbox_freq = QLineEdit("4.0")
+        btn_setbrk = QPushButton('Set')
+        btn_setfreq = QPushButton('Set')
+        lbl_timer = QLabel("0us")
+        self.tbl_porta = MemTable(3, 8)
+        self.tbl_portb = MemTable(3, 8)
+        self.tbl_mem = MemTable(80, 9)
+        lbl_sfr = QLabel("SFR")
+        tableData : list
         
-        self.menubar = QMenuBar(self)
+        menubar = QMenuBar(self)
         file_menu = QMenu("Datei", self)
         open_action = file_menu.addAction("Öffnen")
-        self.menubar.addMenu(file_menu)
+        menubar.addMenu(file_menu)
+        
+        self.tbl_porta.setHorizontalHeaderLabels(['RA 7','RA 6','RA 5','RA 4','RA 3','RA 2','RA 1','RA 0'])
+        self.tbl_porta.setVerticalHeaderLabels(['TRIS','i/o','RA'])
+        self.tbl_portb.setHorizontalHeaderLabels(['RB 7','RB 6','RB 5','RB 4','RB 3','RB 2','RB 1','RB 0'])
+        self.tbl_portb.setVerticalHeaderLabels(['TRIS','i/o','RB'])
+        self.tbl_mem.setHorizontalHeaderLabels(['Adress', 'Bit 0', 'Bit 1', 'Bit 2', 'Bit 3', 'Bit 4', 'Bit 5', 'Bit 6', 'Bit 6', 'Full Value'])
+        
+        lay_reg.addWidget(self.tbl_porta)
+        lay_reg.addWidget(self.tbl_portb)
+        lay_reg.addWidget(self.tbl_mem)
+        
+        lay_code.addWidget(lbl_code)
+        
+        lay_runctrl.addWidget(btn_step)
+        lay_runctrl.addWidget(btn_run)
+        lay_runctrl.addWidget(btn_stop)
+        lay_runctrl.addWidget(btn_reset)
+        
+        lay_brk.addWidget(txtbox_brk)
+        lay_brk.addWidget(btn_setbrk)
+        
+        lay_freq.addWidget(txtbox_freq)
+        lay_freq.addWidget(btn_setfreq)
+        
+        lay_runctrl.addLayout(lay_brk)
+        lay_runctrl.addLayout(lay_freq)
+        
+        lay_runctrl.addWidget(lbl_timer)
+        
+        lay_main.addLayout(lay_reg)
+        lay_main.addLayout(lay_code)
+        lay_main.addLayout(lay_runctrl)
 
-        self.lay_reg.addWidget(self.tbl_porta)
-        self.lay_reg.addWidget(self.tbl_portb)
-        self.lay_reg.addWidget(self.tbl_mem)
+        widg_main.setLayout(lay_main)
+        widg_main.setGeometry(0, menubar.height(), 0, 0)
         
-        self.lay_code.addWidget(self.lbl_code)
-        
-        self.lay_runctrl.addWidget(self.btn_step)
-        self.lay_runctrl.addWidget(self.btn_run)
-        self.lay_runctrl.addWidget(self.btn_stop)
-        self.lay_runctrl.addWidget(self.btn_reset)
-        
-        self.lay_brk.addWidget(self.txtbox_brk)
-        self.lay_brk.addWidget(self.btn_setbrk)
-        
-        self.lay_freq.addWidget(self.txtbox_freq)
-        self.lay_freq.addWidget(self.btn_setfreq)
-        
-        self.lay_runctrl.addLayout(self.lay_brk)
-        self.lay_runctrl.addLayout(self.lay_freq)
-        
-        self.lay_runctrl.addWidget(self.lbl_timer)
-        
-        self.lay_main.addLayout(self.lay_reg)
-        self.lay_main.addLayout(self.lay_code)
-        self.lay_main.addLayout(self.lay_runctrl)
-
-        self.setLayout(self.lay_main)
+        self.setMenuBar(menubar)
+        self.setCentralWidget(widg_main)
+        menubar.raise_()
         self.resize(800, 600)
+        
 
     def init_window(self):
         self.create_window()
@@ -114,5 +136,8 @@ class MainWindow(QWidget):
 
     def setMemData(self):
         self.tbl_mem.setData(self.p.mem)
+        self.tbl_porta.setPortData(self.p.mem, 5)
+        self.tbl_portb.setPortData(self.p.mem, 6)
+        
 
     
